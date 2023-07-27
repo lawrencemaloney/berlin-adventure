221e5efd7b9d930e15ffddf3fbfe33fc4d999427
Larry Maloney <55635508+lawrencemaloney@users.noreply.github.com>
2023-07-27 16:27:58 -0500

    Change vite com fig to not send image files

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+// export default defineConfig({
+  // plugins: [react()],
+
+// })
+
+// CODE BELOW allows Public Folder contents to be served but not included in DIST 
+
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      src: "./src",
+    },
+  },
+  publicDir: command === "serve" ? "public" : false,
+}));
